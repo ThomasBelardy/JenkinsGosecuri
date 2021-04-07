bd47492cf710dc13d0355457d6fb11567aa8b444
ThomasBelardy <thomas.belardy@protonmail.com>
2021-04-07 10:13:24 +0200

    premier commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        try
+        {
+            // Le fichier d'entrée
+            FileInputStream file = new FileInputStream("file.txt");
+            Scanner scanner = new Scanner(file);
+
+            //renvoie true s'il y a une autre ligne à lire
+            while(scanner.hasNextLine())
+            {
+                System.out.println(scanner.nextLine());
+            }
+            scanner.close();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+
+
+    }
+}
